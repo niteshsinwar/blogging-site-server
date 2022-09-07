2cd494355a63b9ec68622891e9863148af6806a7
nitesh sinwar <niteshsinwar@gmail.com>
2022-09-07 13:39:20 +0530

    first commit

---
+++ models/post.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+const { ObjectId } = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    likes:{type:Number},
+    content: {},
+    categories: [{ type: ObjectId, ref: "Category" }],
+    published: { type: Boolean, default: true },
+    postedBy: { type: ObjectId, ref: "User" },
+    featuredImage: { type: ObjectId, ref: "Media" },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Post", postSchema);
